49c19fa3a450e24fc17a49645a9ad65b17d4a8e7
freetoliv <freegotonas@gmail.com>
2022-11-18 15:11:45 +0800

    add some picture and music html

---
+++ vue-my-firproject/src/App.vue
@@ -0,0 +1,29 @@
+<script>
+  import Modal from './Modal.vue'
+  import Todo from './Todo.vue'
+
+  export default {
+    components: {
+      Modal,
+      Todo
+    },
+    data() {
+      return {
+        showModal: false
+      }
+    }
+  }
+</script>
+
+<template>
+  <button id="show-modal" @click="showModal = true">Show Modal</button>
+
+  <Teleport to="body">
+    <modal :show="showModal" @close="showModal = false">
+       <template #body>
+        <todo></todo>
+       </template>
+       
+    </modal>
+  </Teleport>
+</template>
